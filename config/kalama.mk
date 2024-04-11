80516b3386eea2823ca8b90b9d5f142442f13c52
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-04-11 17:29:42 +0200

    ARM: dts: msm: Import Xiaomi camera devicetree changes
     * from sheng-u-oss
    
    Change-Id: Ia121fd095acc9278b99f2e83a8ff6af06073ad23

--- config/kalama.mk
+++ config/kalama.mk
@@ -4,4 +4,11 @@ dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
 								kalama-camera-sensor-qrd.dtbo \
 								kalama-camera-sensor-hdk.dtbo \
 								kalama-sg-hhg-camera.dtbo \
-								kalama-sg-hhg-camera-sensor.dtbo
+								kalama-sg-hhg-camera-sensor.dtbo \
+								nuwa-sm8550-camera-sensor.dtbo \
+								fuxi-sm8550-camera-sensor.dtbo \
+								socrates-sm8550-camera-sensor.dtbo \
+								ishtar-sm8550-camera-sensor.dtbo \
+								babylon-sm8550-camera-sensor.dtbo \
+								vermeer-sm8550-camera-sensor.dtbo \
+								sheng-sm8550-camera-sensor.dtbo
